04c0c018c9d36ead9466c4a7a2d99f2c86c152b6
unknown <bengkelgadget.l2@gmail.com>
2026-06-11 09:58:48 +0800

    chore: add Capacitor Android platform and dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.siwarung.app',
+  appName: 'SIWARUNG',
+  webDir: 'dist'
+};
+
+export default config;
